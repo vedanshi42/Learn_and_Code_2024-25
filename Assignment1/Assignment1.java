0a01819a11807419ac87eaad4baaa9a808473dd5
vedanshi42 <vedanshidixit4201@gmail.com>
2024-12-10 08:31:18 +0530

    Adding assignment1 files

---
+++ Assignment1/Assignment1.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter country code: ");
+        String countryCode = scanner.nextLine().toUpperCase();
+
+        List<String> adjacentCountries = findAdjacentCountries(countryCode);
+
+        if (adjacentCountries != null) {
+            Collections.sort(adjacentCountries);
+            System.out.println(adjacentCountries);
+        } else {
+            System.out.println("Country code not found.");
+        }
+    }
+
+    public static List<String> findAdjacentCountries(String countryCode) {
+        Map<String, List<String>> countries = new HashMap<>();
+        countries.put("IN", Arrays.asList("Pakistan", "China", "Nepal", "Bhutan", "Bangladesh", "Myanmar", "Afghanistan", "Maldives", "Sri Lanka"));
+        countries.put("US", Arrays.asList("Canada", "Mexico"));
+        countries.put("NZ", Arrays.asList("Australia"));
+        countries.put("GB", Arrays.asList("Ireland", "France", "Netherlands"));
+        countries.put("FR", Arrays.asList("Belgium", "Luxembourg", "Switzerland", "Spain", "Italy", "Germany", "Monaco", "Brazil", "Andorra", "Suriname"));
+
+        return countries.get(countryCode);
+    }
+}
---
+++ Assignment1/Naming_conventions assignment.py
@@ -0,0 +1,87 @@
+import random
+
+
+def rolling_dice(sides):
+    output_number = random.randint(1, sides)
+    return output_number
+
+
+def dice():
+    sides = 6
+    roll = True
+    while roll:
+        quit_flag = input("Ready to roll? Enter Q to Quit")
+        if quit_flag.lower() != "q":
+            number_after_rolling = rolling_dice(sides)
+            print("You have rolled a", number_after_rolling)
+        else:
+            roll = False
+
+
+dice()
+
+
+# Assignment 2: The below program is to guess the correct number between 1 to 100
+def correct_number(input_number):
+    if input_number.isdigit() and 1 <= int(input_number) <= 100:
+        return True
+    else:
+        return False
+
+
+def guess_the_number():
+    generated_number = random.randint(1, 100)
+    guessed_correctly = False
+    guessed_number = input("Guess a number between 1 and 100:")
+    number_of_guesses = 0
+    while not guessed_correctly:
+        if not correct_number(guessed_number):
+            guessed_number = input("I wont count this one Please enter a number between 1 to 100")
+            continue
+        else:
+            number_of_guesses += 1
+            guessed_number = int(guessed_number)
+
+        if guessed_number < generated_number:
+            guessed_number = input("Too low. Guess again")
+        elif guessed_number > generated_number:
+            guessed_number = input("Too High. Guess again")
+        else:
+            print("You guessed it in", number_of_guesses, "guesses!")
+            guessed_correctly = True
+
+
+guess_the_number()
+
+# Assignment 3: The below program is to check whether the number is Armstrong number or not
+
+
+def check_Armstrong(input_number):
+    # Initializing Sum and Number of Digits
+    sum_of_digits = 0
+    number_of_digits = 0
+
+    # Calculating Number of individual digits
+    number_to_check = input_number
+    while number_to_check > 0:
+        number_of_digits += 1
+        number_to_check = number_to_check // 10
+
+    # Finding Armstrong Number
+    number_to_check = input_number
+    for n in range(1, number_to_check + 1):
+        remainder = number_to_check % 10
+        sum_of_digits = sum_of_digits + (remainder ** number_of_digits)
+        number_to_check //= 10
+    return sum_of_digits
+
+
+# End of Function
+
+# User Input
+input_number = int(input("\nPlease Enter the Number to Check for Armstrong: "))
+
+if (input_number == check_Armstrong(input_number)):
+    print("\n %d is Armstrong Number.\n" % input_number)
+else:
+    print("\n %d is Not a Armstrong Number.\n" % input_number)
