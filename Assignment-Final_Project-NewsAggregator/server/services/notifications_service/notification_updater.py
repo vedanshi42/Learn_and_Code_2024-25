45b5d5efbf3a294693f156f7c34124c08e75e25a
vedanshi42 <vedanshidixit4201@gmail.com>
2025-07-01 08:54:09 +0530

    modifying notification service file to include and search only based on user email

--- Assignment-Final_Project-NewsAggregator/server/services/notifications_service/notification_updater.py
+++ Assignment-Final_Project-NewsAggregator/server/services/notifications_service/notification_updater.py
@@ -1,4 +1,4 @@
-from server.repositories.article_repository import SearchArticleRepository
+from server.repositories.search_article_repository import SearchArticleRepository
 from server.repositories.keyword_repository import KeywordRepository
 from server.repositories.category_repository import CategoryRepository
 from server.repositories.user_repository import UserRepository
@@ -17,6 +17,6 @@ class NotificationsUpdater:
         users = self.user_repo.get_all_users()
         for user in users:
             keywords = self.keyword_repo.get_keywords_for_user(user['email'])
-            categories = self.category_repo.get_user_categories(user['user_id'])
+            categories = self.category_repo.get_user_categories(user['email'])
             matched_articles = self.search_article_repo.find_articles_by_category_or_keyword(categories, keywords)
             self.notification_repo.replace_notifications_for_user(user['user_id'], matched_articles)
