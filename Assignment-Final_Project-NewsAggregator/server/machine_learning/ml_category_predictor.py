2d82c694411b6cc7a3aa99595a972b5b658a11a1
vedanshi42 <vedanshidixit4201@gmail.com>
2025-07-05 13:53:09 +0530

    moving ml model and training file to machine learning folder

---
+++ Assignment-Final_Project-NewsAggregator/server/machine_learning/ml_category_predictor.py
@@ -0,0 +1,17 @@
+import os
+import joblib
+from server.models.article import Article
+from server.interfaces.i_category_predictor import ICategoryPredictor
+
+
+class MLCategoryPredictor(ICategoryPredictor):
+    def __init__(self):
+        model_path = os.path.join("docs", "trained_category_model.joblib")
+        if not os.path.exists(model_path):
+            raise FileNotFoundError("Trained model not found. Run train_category_model.py first.")
+        self.model, self.vectorizer = joblib.load(model_path)
+
+    def predict(self, article: Article):
+        combined_text = f"{article.title} {article.content}".lower()
+        X = self.vectorizer.transform([combined_text])
+        return self.model.predict(X)[0]
